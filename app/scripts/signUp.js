cf33f6c204df8c436d937a2a5e8fd44b1bea4063
winsan-zhang <zhangxiaochang0@sina.comm>
2016-10-28 17:32:55 +0800

    modify signUp layout

--- app/scripts/signUp.js
+++ app/scripts/signUp.js
@@ -25,7 +25,9 @@ class SignUp extends React.Component{
                         <form action="" className="sign-form">
                             <h3 className='form-title'>注册</h3>
                             <input type="text" className="username-input form-input" id="username" placeholder="请输入用户名/邮箱/手机号" />
+                            <span className='tips'>123</span>
                             <input type="password" className="password-input form-input" id="password" placeholder="密码"/>
+                            <span className='tips'>123</span>
                             <input type="text" className='form-input verify-code' placeholder="请出入验证码"/>
                             <input type="submit" className='submit-btn form-btn' value="注册" />
                             <button className='form-btn signIn-btn'><a href="" className='form-btn-a'>登录</a></button>
